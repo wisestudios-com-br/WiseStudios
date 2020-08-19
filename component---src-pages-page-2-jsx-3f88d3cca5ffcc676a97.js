7e610b2881b5a828a74180609dc816a770d6ac30
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-08-19 19:44:08 -0300

    Updates

---
+++ component---src-pages-page-2-jsx-3f88d3cca5ffcc676a97.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{caWp:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),c=n("Wbzz"),l=n("L12J");t.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-jsx-3f88d3cca5ffcc676a97.js.map
